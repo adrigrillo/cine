f81aff241e55744f06cbe5d3a051344bf500f3c9
Adrian Rodriguez Grillo <100316457@alumnos.uc3m.es>
2016-04-11 01:22:53 +0200

    Logrado que funcione el obtener datos del servidor

--- app/app.js
+++ app/app.js
@@ -1,11 +1,25 @@
 (function() {
-'use strict';
 
 	// Declare app level module which depends on views, and components
-	var app = angular.module('cine', ['ngRoute', 'firebase'])
+	var cine = angular.module("cine", ["firebase"]);
+	cine.factory("obtenerDatos", ["$firebaseArray", function($firebaseArray){
+		var ref = new Firebase("https://cinedsi.firebaseio.com/peliculas");
+		return $firebaseArray(ref);
+	}]);
 
-	.config(['$routeProvider', function($routeProvider) {
-
+	cine.controller("verPeliculas", ["$scope", "obtenerDatos", function($scope, obtenerDatos) {
+    	console.log('Array de firebase: %O', obtenerDatos);
+    	$scope.peliculas = obtenerDatos;
+    	$scope.peliculas.$loaded(function() {
+      		if ($scope.peliculas.length === 0) {
+        		$scope.peliculas.$add({
+        			year: 2014,
+        			titulo: "Esto esta mal",
+        			productora: "Jesus enterteinment"
+		        });
+      		}
+    	});
+    	console.log('Array de final: %O', $scope.peliculas);
 	}]);
 
 })();
