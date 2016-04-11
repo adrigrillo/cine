e6ad0f1050212c375b4ee2cebc1d0e6c964c7882
Adrian Rodriguez Grillo <100316457@alumnos.uc3m.es>
2016-04-11 16:57:48 +0200

    nsjf

---
+++ app/app.js
@@ -0,0 +1,30 @@
+(function() {
+
+	// Declare app level module which depends on views, and components
+	var cine = angular.module("cine", ["firebase", "database"]);
+
+    /* Guarda como un array los datos de las películas obtenidas.
+        Por ejemplo, si usamos "ng-controller = verPeliculas as peliculas" devuelve el array de todas
+        las películas en 'peliculas' pudiendo acceder a las posiciones como peliculas[0] y dentro de
+        esto a los atributos 'peliculas[0].titulo' */
+	cine.controller("verPeliculas", ["$scope", "obtenerPeliculas", function($scope, obtenerPeliculas) {
+    	$scope.peliculas = obtenerPeliculas;
+        //En caso de error se informa por consola
+    	$scope.peliculas.$loaded(function() {
+      		if ($scope.peliculas.length === 0) {
+        		console.error("No se han obtenido datos")
+      		}
+    	});
+	}]);
+
+    cine.controller("verCines", ["$scope", "obtenerCines", function($scope, obtenerCines) {
+        $scope.cines = obtenerCines;
+        //En caso de error se informa por consola
+        $scope.cines.$loaded(function() {
+            if ($scope.cines.length === 0) {
+                console.error("No se han obtenido datos")
+            }
+        });
+    }]);
+
+})();
---
+++ app/database.js
@@ -0,0 +1,16 @@
+(function() {
+	/* EN ESTA CLASE VAMOS A RECOGER LOS DIFERENTES DATOS DE LA BASE DE DATOS */
+	var cine = angular.module("database", ["firebase"]);
+
+	// Metodo para obtener los datos de las películas, devuelve un array con las películas
+	cine.factory("obtenerPeliculas", ["$firebaseArray", function($firebaseArray){
+		var ref = new Firebase("https://cinedsi.firebaseio.com/peliculas");
+		return $firebaseArray(ref);
+	}]);
+
+	// Metodo para obtener los datos de los cines, devuelve un array con las cines
+	cine.factory("obtenerCines", ["$firebaseArray", function($firebaseArray){
+		var ref = new Firebase("https://cinedsi.firebaseio.com/cines");
+		return $firebaseArray(ref);
+	}]);
+})();
