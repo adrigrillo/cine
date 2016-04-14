c342193e1912df688cbd7ba0f1b3fda7c46173e7
Adrian Rodriguez Grillo <100316457@alumnos.uc3m.es>
2016-04-14 12:10:53 +0200

    Para enseñar en sprint

--- app/app.js
+++ app/app.js
@@ -19,7 +19,6 @@
         $scope.tab2 = 'Cines';
     });
 
-
     /* Guarda como un array los datos de las películas obtenidas.
         Por ejemplo, si usamos "ng-controller = verPeliculas as peliculas" devuelve el array de todas
         las películas en 'peliculas' pudiendo acceder a las posiciones como peliculas[0] y dentro de
@@ -29,7 +28,7 @@
         //En caso de error se informa por consola
     	$scope.peliculas.$loaded(function() {
       		if ($scope.peliculas.length === 0) {
-        		console.error("No se han obtenido datos")
+        		console.error("No se han obtenido datos");
       		}
     	});
 	}]);
@@ -39,7 +38,7 @@
         //En caso de error se informa por consola
         $scope.cines.$loaded(function() {
             if ($scope.cines.length === 0) {
-                console.error("No se han obtenido datos")
+                console.error("No se han obtenido datos");
             }
         });
     }]);
