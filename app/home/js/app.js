ea6fb73cccf1bd993f2796a7b85a64e955ed0adf
Jorge Frias <100317118@alumnos.uc3m.es>
2016-04-14 23:54:25 +0200

    En ello

--- app/home/js/app.js
+++ app/home/js/app.js
@@ -1,11 +1,17 @@
-var app = angular.module("BlankApp", ['ngMaterial'])
-  .config(function($mdThemingProvider) {
-    $mdThemingProvider.theme('custom') /* color palette */
-      .primaryPalette('indigo')
-      .accentPalette('yellow')
-      .warnPalette('orange')
-      .backgroundPalette('lime', {
-        'default': '600'
+(function() {
+  // Declare app level module which depends on views, and components
+  var cine = angular.module("cine", ["ngMaterial", "firebase", "database", "user"]);
+
+  /* Configurar tema */
+  cine.config(function($mdThemingProvider) {
+    $mdThemingProvider.theme("default") /* color palette */
+      .primaryPalette("indigo")
+      .accentPalette("yellow")
+      .warnPalette("orange")
+      .backgroundPalette('indigo', {
+        'default': '50'
       });
-    $mdThemingProvider.setDefaultTheme('custom');
+    $mdThemingProvider.setDefaultTheme('default');
   });
+
+})();
