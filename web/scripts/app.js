5310cec5affbae63038065a10290e2cb556e6442
Adrian Rodriguez Grillo <100316457@alumnos.uc3m.es>
2016-04-21 10:41:50 +0200

    Eliminado materialmaps y añadido lo necesario para hacerlo con fotos

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -63,6 +63,15 @@
         }
     }]);
 
+    //Controlador de los mapas de la aplicacion
+    app.controller('MyController', function(NgMap) {
+        NgMap.getMap().then(function(map){
+            console.log(map.getCenter());
+            console.log('markers', map.markers);
+            console.log('shapes', map.shapes);
+        });
+    });
+
     // Controlador para los pagos de la aplicación
     cine.controller('DemoCtrl', function($scope) {
         $scope.user = {
