2ca26bd7b2cacc91814032c32e72078b67ee57e8
Luis Buceta <luisbu.ojeda@gmail.com>
2016-04-23 21:30:23 +0200

    Realizado página info pelis

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -1,18 +1,17 @@
 (function() {
 
-	// Declare app level module which depends on views, and components
-	var cine = angular.module("cine", ["ngMaterial","firebase", "database"]);
+    // Declare app level module which depends on views, and components
+    var cine = angular.module("cine", ["ngMaterial", "firebase", "database"]);
 
     /* Configurar tema */
     cine.config(function($mdThemingProvider) {
-        $mdThemingProvider.theme("default")                   /* color palette */
+        $mdThemingProvider.theme("default") /* color palette */
             .primaryPalette("indigo")
             .accentPalette("yellow")
-            .warnPalette("pink")
-			.backgroundPalette('indigo',{
-				'default': '50'
-			}
-		);
+            .warnPalette("yellow")
+            .backgroundPalette('indigo', {
+                'default': '50'
+            });
     });
 
     /* Controlador de las tabs */
@@ -27,22 +26,22 @@
         las películas en 'peliculas' pudiendo acceder a las posiciones como peliculas[0] y dentro de
         esto a los atributos 'peliculas[0].titulo'
     */
-	cine.controller("verPeliculas", ["$scope", "obtenerPeliculas", function($scope, obtenerPeliculas) {
-    	$scope.peliculas = obtenerPeliculas;
+    cine.controller("verPeliculas", ["$scope", "obtenerPeliculas", function($scope, obtenerPeliculas) {
+        $scope.peliculas = obtenerPeliculas;
         //En caso de error se informa por consola
-    	$scope.peliculas.$loaded(function() {
-      		if ($scope.peliculas.length === 0) {
-        		console.error("No se han obtenido datos");
-      		}
-    	});
-	}]);
+        $scope.peliculas.$loaded(function() {
+            if ($scope.peliculas.length === 0) {
+                console.error("No se han obtenido datos");
+            }
+        });
+    }]);
 
     // Guarda un array de datos de todos los cines obtenidos
     cine.controller("verCines", ["$scope", "obtenerCines", function($scope, obtenerCines) {
         $scope.cines = obtenerCines;
         //En caso de error se informa por consola
-        $scope.cines.$loaded(function(){
-            if ($scope.cines.length === 0){
+        $scope.cines.$loaded(function() {
+            if ($scope.cines.length === 0) {
                 console.error("No se han obtenido datos");
             }
         });
@@ -50,17 +49,52 @@
 
     // Guarda en un objeto el cine requerido
     cine.controller("verCine", ["$scope", "cine", function($scope, cine) {
-        function nombreCine(nombre){
+        function nombreCine(nombre) {
             $scope.cine = cine(nombre);
             console.error("Ha entrado");
-            $scope.cine.$loaded(function(){
+            $scope.cine.$loaded(function() {
                 console.error("Ha funcionado");
-                if ($scope.cines === null){
+                if ($scope.cines === null) {
                     console.error("No se han obtenido datos");
                 }
             });
         }
     }]);
+
+    cine.controller("CuadroInfo", function($scope, $mdDialog, $mdMedia) {
+        $scope.status = '  ';
+        $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
+        $scope.showConfirm = function(ev) {
+            // Appending dialog to document.body to cover sidenav in docs app
+            var confirm = $mdDialog.confirm({
+                controller: DialogController,
+                templateUrl: 'infoPeli.html',
+                clickOutsideToClose: true,
+                scope: $scope.$new(),
+                parent: angular.element(document.body),
+                targetEvent: ev,
+            })
+            $mdDialog.show(confirm).then(function() {
+                $scope.status = 'Confirm resolved';
+                $scope.codeRunningBeforeResolve = 'code only runs after resolve';
+            });
+
+            $scope.codeRunningBeforeResolve = 'code is running before resolve!';
+        };
+
+    });
+
+    function DialogController($scope, $mdDialog) {
+        $scope.hide = function() {
+            $mdDialog.hide();
+        };
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
+        $scope.answer = function(answer) {
+            $mdDialog.hide(answer);
+        };
+    }
 
     // Controlador para los pagos de la aplicación
     cine.controller('DemoCtrl', function($scope) {
@@ -79,21 +113,29 @@
             csc: 'XXX',
         };
         $scope.provincias = ('Madrid,Asturias,Alicante,Badajoz,Ciudad Real, La Coruña,Huelva,Barcelona').split(',').map(function(provincia) {
-            return {abbrev: provincia};
-          });
+            return {
+                abbrev: provincia
+            };
+        });
 
         $scope.anos = ('2016 2017 2018 2019 2020 2021 2022 2023').split(' ').map(function(años) {
-            return {abbrev: años};
+            return {
+                abbrev: años
+            };
         });
 
         $scope.meses = ('1 2 3 4 5 6 7 8 9 10 11 12').split(' ').map(function(meses) {
-            return {abbrev: meses};
+            return {
+                abbrev: meses
+            };
         });
 
         $scope.modopago = true;
 
         $scope.pagos = ('PayPal, Tarjeta').split(',').map(function(pago) {
-            return {abbrev: pago};
+            return {
+                abbrev: pago
+            };
         });
     });
 })();
